0a86ffbe1c058a05c82ad8658aa479424a14d7a7
Shujinko53 <shujinko53@yandex.ru>
2021-04-10 15:53:28 +0300

    creating the app

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './App';
+import App from './components/app/';
 import reportWebVitals from './reportWebVitals';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <Clock />
-            </div>
-        )
-    }
-}
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div className="wrapper">
+//                 <Clock />
+//                 <Button />
+//             </div>
+//         )
+//     }
+// }
 
 ReactDOM.render(
     <React.StrictMode>
-    <Clock />
+    <App />
     </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
